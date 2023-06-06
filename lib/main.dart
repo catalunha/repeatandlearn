244f443f5628cafda17dd96de6a9fed204ed4ce8
catalunha <catalunha.mj@gmail.com>
2023-06-06 14:19:17 -0300

    observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
+import 'app/observer.dart';
 
 void main() {
-  runApp(const ProviderScope(child: App()));
+  runApp(const ProviderScope(
+    // observers: [Logger()],
+    child: App(),
+  ));
 }
